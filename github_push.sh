3ff78a72f2fead15230c1c847e9ce8c425de59ec
tn <tangnayx@163.com>
2020-03-23 16:56:11 +0800

    写文章的blog全部备份

--- github_push.sh
+++ github_push.sh
@@ -1,6 +1,7 @@
 hexo clean
+cp -r source/_posts/blog.github.io/images/*   source/images/
 git add -A
 git commit -m '写文章的blog全部备份'
-git push -u github_blog github_blog
 git pull --rebase github_blog github_blog
+git push -u github_blog github_blog
 hexo g -d
