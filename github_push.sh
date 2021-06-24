9e5816fb77f685fe1342a142089c0992a187ce53
magic <tangnayx@163.com>
2021-06-24 17:04:19 +0800

    修改内容提交到分支dev

--- github_push.sh
+++ github_push.sh
@@ -8,18 +8,14 @@ git push -u origin dev
 
 git checkout github_blog
 git pull --rebase origin github_blog
-
 git merge dev
 git status
+
 hexo clean
 cp -r source/_posts/blog.github.io/images/*   source/images/
 git add .
 git commit -m “修改内容提交到分支github_blog”
 git pull --rebase origin github_blog
 git push -u origin github_blog
-#git add -A
-#git commit -m '写文章的blog全部备份'
-#git pull --rebase github_blog github_blog
-#git push -u github_blog github_blog
 
 hexo g -d
